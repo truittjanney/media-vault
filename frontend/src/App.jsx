c6108910bdbddff5c5ac2470a0ce4d57ff220870
truittjanney <truittjanney93@gmail.com>
2026-06-19 20:07:20 -0500

    build: added generic styling css classes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { LoginPage } from "./pages/LoginPage";
@@ -7,13 +9,15 @@ import { AlbumDetailPage } from "./pages/AlbumDetailPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/albums" element={<AlbumsPage />} />
-      <Route path="/albums/:id" element={<AlbumDetailPage />} />
-    </Routes>
+    <div className="app-shell">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/albums" element={<AlbumsPage />} />
+        <Route path="/albums/:id" element={<AlbumDetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
